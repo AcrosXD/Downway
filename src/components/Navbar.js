7f24fb89f9e1d3efb2a818bc4b21fff264577b89
AcrosX <51994441+AcrosXD@users.noreply.github.com>
2022-02-05 13:04:23 +0100

    Starting create nav bar with a catastrophic css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,79 +2,30 @@ import React from "react"
 import { NavLink } from "react-router-dom";
 import "../css/navbar.css"
 import "https://kit.fontawesome.com/b99e675b6e.js"
-import "../scripts/navbar"
 
 const Navbar = () => {
     return(
-
-        <div className="wrapper hover_collapse">
-            <div className="top_navbar">
-                <div className="logo">Downway</div>
-                <div className="menu">
-                    <div className="hamburger">
-                        <i className="fas fa-bars"></i>
-                    </div>
-                    <div className="profile_wrap">
-                        <div className="profile">
-                            <img src="profile.png" alt="profile_pic" />
-                                <span className="name">username</span>
-                                <span className="icon">
-						<i className="fas fa-angle-down"></i>
-					</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div className="sidebar">
-                <div className="sidebar_inner">
-                    <ul>
-                        <li>
-                            <NavLink exact to="/dashboard">
-                            <a>
-                                <span className="icon"><i className="fas fa-home"></i></span>
-                                <span className="text">Home</span>
-                            </a>
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink exact to="/catalog">
-                            <a>
-                                <span className="icon"><i className="fas fa-file-video"></i></span>
-                                <span className="text">Catalogue</span>
-                            </a>
-                                </NavLink>
-                        </li>
-                        <li>
-                            <NavLink exact to="/server">
-                            <a>
-                                <span className="icon"><i className="fas fa-server"></i></span>
-                                <span className="text">Server Series</span>
-                            </a>
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink exact to="/profile">
-                            <a>
-                                <span className="icon"><i className="fas fa-user"></i></span>
-                                <span className="text">Profile</span>
-                            </a>
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink exact to="/settings">
-                            <a>
-                                <span className="icon"><i className="fas fa-sliders-h"></i></span>
-                                <span className="text">Options</span>
-                            </a>
-                            </NavLink>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+        <div className="navItem navbarBox">
+            <ul>
+                <p>Downway</p>
+                <NavLink exact to="/dashboard">
+                    <li><i className="fas fa-home fa-2x"><a> Home</a></i></li>
+                </NavLink>
+                <NavLink exact to="/catalog">
+                    <li><i className="fas fa-list-ul fa-2x"><a> Catalog</a></i></li>
+                </NavLink>
+                <NavLink exact to="/server">
+                    <li><i className="fas fa-server fa-2x"><a> Server</a></i></li>
+                </NavLink>
+                <NavLink exact to="/settings">
+                    <li><i className="fas fa-cog fa-2x"><a> Settings</a></i></li>
+                </NavLink>
+                <NavLink exact to="/profile">
+                    <li><i className="fas fa-user fa-2x"><a> Profile</a></i></li>
+                </NavLink>
+            </ul>
         </div>
-
-                )
+    )
 }
 
 export default Navbar;
--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,10 +1,11 @@
 import React from "react"
+import { NavLink } from "react-router-dom";
 
 const NotFound = () => {
     return(
         <div>
             <h1>Erreur 404</h1>
-            <p>Page non trouvé <a href=''> Revenir au dashboard</a></p>
+            <p>Page non trouvé <NavLink exact to="/dashboard"><a> Revenir au dashboard</a></NavLink></p>
         </div>
     )
 }
